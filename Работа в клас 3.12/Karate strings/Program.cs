60938239e9950cb9e98d050cf9d490999b70044a
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-07 13:53:14 +0200

    school task

---
+++ Работа в клас 3.12/Karate strings/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+string karate = Console.ReadLine();
+StringBuilder output = new StringBuilder();
+int lengthR = 0;
+for(int i=0;i<karate.Length;i++)
+{
+    if(karate[i]=='>')
+    {
+        lengthR = int.Parse(karate[i + 1].ToString());
+    }
+}
---
+++ Работа в клас 3.12/Имейл/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int SumLetters(string str)
+{
+    int sum = 0;
+    for(int i=0;i<=str.Length;i++)
+    {
+        sum += (int)str[i];
+    }
+    return sum;
+}
+string email = Console.ReadLine();
+string[] words = email.Split('@');
+if(SumLetters(words[0])>=SumLetters(words[1]))
+{
+    Console.WriteLine("Call her!");
+}
+else
+{
+    Console.WriteLine("She is not the one.");
+}
