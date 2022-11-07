aad121d3c6844218cd7963778c5f575018dc34af
tedipatov05 <teodorpatov05@pmgkk.com>
2022-11-07 13:50:35 +0200

    school work

---
+++ Работа в клас 22.11/зад. 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+Random num = new Random();
+Stack<int> stack = new Stack<int>();
+for(int i=0;i<n;i++)
+{
+    int number = num.Next(1, 100);
+    stack.Push(number);
+}
+
---
+++ Работа в клас 22.11/зад.1 Палиндром (със стек)/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Console.ReadLine();
+Stack<char> stack = new Stack<char>();
+foreach (char element in input)
+{
+    stack.Push(element);
+}
+bool isPalindrom = true;
+foreach(char element in input)
+{
+    if(element != stack.Pop())
+    {
+        isPalindrom = false;
+    }
+}
+if(isPalindrom)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
